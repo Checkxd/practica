250c1a00bc9e5f531c19ccb8c73c4d6f85ecd158
check <adriansatalin@gmail.com>
2025-04-08 10:17:00 +0300

    lab20

---
+++ lab20/EveningMovies/Controllers/EveningController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using EveningMovies.Models;
+
+namespace EveningMovies.Controllers
+{
+	public class EveningController : Controller
+	{
+		private static List<Movie> movies = new List<Movie>();
+
+		public ActionResult Index()
+		{
+			return View(movies);
+		}
+
+		public ActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Add(Movie movie)
+		{
+			movie.Id = movies.Count + 1;
+			movie.DateAdded = DateTime.Now;
+			movies.Add(movie);
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Details(int id)
+		{
+			var movie = movies.FirstOrDefault(m => m.Id == id);
+			if (movie == null) return NotFound();
+			return View(movie);
+		}
+	}
+}
---
+++ lab20/EveningMovies/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace EveningMovies.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Genre { get; set; }
+		public string RecommendedBy { get; set; }
+		public DateTime DateAdded { get; set; }
+	}
+}
